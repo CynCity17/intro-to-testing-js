00f16ff140f807d62a66819d17d6059d1de59fb4
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-02-23 11:12:12 -0600

    unit test for edge cases

--- code.js
+++ code.js
@@ -1,8 +1,18 @@
-// helloWorld function 
+// helloWorld function
+function isAnythingButAString(input){
+    return !isNaN(parseFloat(input)) || input === null || typeof input === "boolean" || input === "";
+}
+
+
 function helloWorld() {
     return "Hello, World!";
 }
 
 function sayHello(input) {
+    if (isAnythingButAString(input)) {
+        return ("Hello, World!");
+    } else {
         return (`Hello, ${input}!`);
+    }
 }
+
